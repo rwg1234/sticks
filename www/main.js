82a63fb4180dfe5e1bebb3223736bb98bd3de689
Rory Graham <2460896g@student.gla.ac.uk>
2022-03-12 14:57:21 +0000

    Keep all sprites in the same array, instead of having several. Also fixes #24 and makes it much easier to do #23 Also cleaned some code a little since I had to rewrite it anyway.

--- www/main.js
+++ www/main.js
@@ -30,80 +30,77 @@ function makeBox(t) {
     var x_total = 0;
     var y_total = 0;
     var count = 0;
-    for (var i = 0; i < sticks.length; i++) {
-        if (selected_sticks_indices.has(i)) {
-            x_total += sticks[i].x;
-            y_total += sticks[i].y;
+    for (var i = 0; i < sprites.length; i++) {
+        if (selected_indices.has(i) && sprites[i].type == "stick") {
+            x_total += sprites[i].image.x;
+            y_total += sprites[i].image.y;
             count += 1;
         }
     }
     let x = x_total / count;
     let y = y_total / count;
 
-    var image = t.add.image(x, y, 'basic_png_box').setInteractive();
-    t.input.setDraggable(image);
-    boxes.push(image);
-}
-
-function makeSticks(n, t) {
-    var new_sticks = [];
-    for (var i = 0; i<n; i++) {
-        var placeable = false;
-        while (placeable == false) {
-            placeable = true;
-            var x = randRange(100, game.config.width-200);
-            var y = randRange(100, game.config.height-100);
-            if (sticks.length > 0) {
-               for (var j = 0; j < sticks.length; j++) {
-                   if ((sticks[j].getBounds()).contains(x,y)) {
-                        placeable = false;
-                   }
-               }
-            } else {
-                placeable = true;
-            }
-        }
-        
-        var image = t.add.image(x, y, 'basic_png_twig').setInteractive();
-        //image.setScale(1/5, 1/5);
-
-        image.setAngle(0);//randRange(-15, 15));
-
-        t.input.setDraggable(image);
-
-        new_sticks.push(image);
-
-    }
-    return new_sticks;
+    var new_box = new Sprite("box", x, y);
+    sprites.push(new_box);
 }
 
 function makeStickHere(x, y) {
-    var image = gameObj.add.image(x, y, 'basic_png_twig').setInteractive();
-    gameObj.input.setDraggable(image);
-    sticks.push(image);
+    var new_stick = Sprite("stick", x, y);
+    sprites.push(new_stick);
 }
 
 var gameObj;
-var sticks = [];
-var boxes = [];
-var selected_sticks_indices = new Set();
-var selected_boxes_indices = new Set();
+var sprites = [];
+var selected_indices = new Set();
 var StickCounter = 1;
 var BaseCounter = 3;
-var Lock_unlock = "Unlocked";
 
-function createSticks() {
-    for (var i=0; i<StickCounter; i++){
-        sticks.push(makeSticks(1, gameObj)[0]);
+class Sprite {
+    constructor(type, x, y) {
+        this.type = type;
+        if (type == "stick") {
+            var image = gameObj.add.image(x, y, 'basic_png_twig').setInteractive();
+            gameObj.input.setDraggable(image)
+            this.image = image;
+        } else if (type == "box") {
+            var image = gameObj.add.image(x, y, 'basic_png_box').setInteractive();
+            gameObj.input.setDraggable(image);
+            this.image = image;
+        } else if (type == "stack") {
+            console.log("stack not implemented yet");
+            // TODO: implement stacks
+        } else {
+            console.log("tried to create a Sprite of type " + type);
+        }
     }
 }
 
-function clearSticks() {
-    for (var i = 0; i < sticks.length; i++) {
-        var s = sticks[i];
-        s.destroy();
+function createSticks() {
+    for (var i=0; i<StickCounter; i++){
+        // sticks.push(makeSticks(1, gameObj)[0]);
+        var placeable = false;
+        var x;
+        var y;
+        while (!placeable) {
+            var x = randRange(100, game.config.width-200);
+            var y = randRange(100, game.config.height-100);
+            placeable = true;
+            for (var j = 0; j < sprites.length; j++) {
+                if (sprites[j].image.getBounds().contains(x, y)) {
+                    placeable = false;
+                }
+            }
+        }
+        let new_stick = new Sprite("stick", x, y);
+        sprites.push(new_stick);
     }
-    sticks = [];
+}
+
+function clearAll() {
+    for (var i = 0; i < sprites.length; i++) {
+        sprites[i].image.destroy();
+    }
+    sprites = [];
 }
 
 var is_dragging = false; // to differentiate between clicking and dragging
@@ -127,30 +124,19 @@ function create() {
 
     this.input.on('gameobjectup', function(pointer, gameObject) {
         if (!is_dragging) {
-            if (sticks.includes(gameObject)) {
-                // clicked object is a stick
-                let i = sticks.indexOf(gameObject);
-                if (selected_sticks_indices.has(i)) {
-                    // stick is already selected
-                    selected_sticks_indices.delete(i); // remove from "selected" set
-                    sticks[i].clearTint(); // remove visual effect
-                } else {
-                    // not currently selected
-                    selected_sticks_indices.add(i); // add to "selected" set
-                    sticks[i].setTintFill(0x00FFFF); // add visual effect
-                }
-            } else if (boxes.includes(gameObject)) {
-                // clicked object is a box
-                let i = boxes.indexOf(gameObject);
-                if (selected_boxes_indices.has(i)) {
-                    // box is already selected
-                    selected_boxes_indices.delete(i); // remove from "selected" set
-                    boxes[i].clearTint(); // remove visual effect
-                } else {
-                    // not currently selected
-                    selected_boxes_indices.add(i); // add to "selected" set
-                    boxes[i].setTintFill(0x00FFFF); // add visual effect
-                }
+            // first we need the index of the clicked sprite
+            var i;
+            for (var j = 0; j < sprites.length; j++) {
+                if (sprites[j].image == gameObject) { i = j; }
+            }
+            if (selected_indices.has(i)) {
+                // sprite is already selected
+                selected_indices.delete(i); // remove from "selected" set
+                sprites[i].image.clearTint(); // remove visual effect
+            } else {
+                // not currently selected
+                selected_indices.add(i); // add to "selected" set
+                sprites[i].image.setTintFill(0x00FFFF);
             }
         }
         is_dragging = false;
@@ -159,32 +145,18 @@ function create() {
 }
 
 function deleteSelected() {
-    var new_sticks = [];
-    for (var i = 0; i < sticks.length; i++) {
-        if (selected_sticks_indices.has(i)) {
-            // stick is selected, so it gets deleted
-            sticks[i].destroy();
-            selected_sticks_indices.delete(i); // deselect its index
+    var new_sprites = [];
+    for (var i = 0; i < sprites.length; i++) {
+        if (selected_indices.has(i)) {
+            // sprite is selected, so it gets deleted
+            sprites[i].image.destroy();
+            selected_indices.delete(i); // deselect its index
         } else {
-            // stick is not selected, so it gets kept
-            new_sticks.push(sticks[i]);
+            // sprite is not selected, so it gets kept
+            new_sprites.push(sprites[i]);
         }
     }
-    sticks = new_sticks; // keep the new array of sticks
-
-    // now do the same for the boxes
-    var new_boxes = [];
-    for (var i = 0; i < boxes.length; i++) {
-        if (selected_boxes_indices.has(i)) {
-            // box is selected, so it gets deleted
-            boxes[i].destroy();
-            selected_boxes_indices.delete(i); // deselect its index
-        } else {
-            // box is not selected, so it gets kept
-            new_boxes.push(boxes[i]);
-        }
-    }
-    boxes = new_boxes; // keep the new array of boxes
+    sprites = new_sprites; // keep the new array of sprites
 }
 
 function updateLabels() {
@@ -203,7 +175,15 @@ function incrementSticks(n) {
 }
 
 function incrementBase(n) {
-    if (Lock_unlock == "Unlocked") {
+    // first check if there are any boxes or stacks
+    var locked = false;
+    for (var i = 0; i < sprites.length; i++) {
+        if (sprites[i].type == "box" || sprites[i].type == "stack") {
+            locked = true;
+        }
+    }
+
+    if (!locked) {
         BaseCounter += n;
         if(BaseCounter <1 ) {
             BaseCounter = 1;
@@ -212,49 +192,71 @@ function incrementBase(n) {
             BaseCounter = 12;
         }
     } else {
-        alert("You cannot change the group size when other groups exist");
+        alert("You can't change the group size when there are boxes or stacks.");
     }
-    updateLabels();
-}
-
-function lock(value) {
-    Lock_unlock = value;
     updateLabels();
 }
 
 function groupSelected() {
-    if (selected_sticks_indices.size == BaseCounter && selected_boxes_indices.size == 0) {
+    var num_selected_sticks = 0;
+    var num_selected_boxes = 0;
+    var num_selected_stacks = 0;
+    for (var i = 0; i < sprites.length; i++) {
+        if (selected_indices.has(i)) {
+            if (sprites[i].type == "stick") {
+                num_selected_sticks++;
+            } else if (sprites[i].type == "box") {
+                num_selected_boxes++;
+            } else if (sprites[i].type == "stack") {
+                num_selected_stacks++;
+            }
+        }
+    }
+    if (num_selected_sticks == BaseCounter && num_selected_boxes == 0 && num_selected_stacks == 0) {
         makeBox(gameObj);
         deleteSelected();
+    } else if (num_selected_sticks == 0 && num_selected_boxes == BaseCounter && num_selected_stacks == 0) {
+        // TODO: make a stack
     } else {
-        alert("Wrong number of sticks");
-    }
-    if (boxes.length == 1) {
-        lock("Locked");
+        alert("You have to select " + convert(BaseCounter) + " sticks, or " + convert(BaseCounter) + " boxes to group them.");
     }
 }
 
 function ungroupSelected() {
-    // first make the new sticks
-    for (var i = 0; i < boxes.length; i++) {
-        if (selected_boxes_indices.has(i)) {
-            // make as many sticks as BaseCounter says
-            for (var j = 0; j < BaseCounter; j++) {
-                // place the stick randomly within the box's footprint
-                let x = boxes[i].x + (Math.random()-0.5)*boxes[i].width;
-                let y = boxes[i].y + (Math.random()-0.5)*boxes[i].height;
-                makeStickHere(x, y);
+    // first make the new sticks and boxes
+    for (var i = 0; i < sprites.length; i++) {
+        if (selected_indices.has(i)) {
+            if (sprites[i].type == "box") {
+                // make as many sticks as BaseCounter says
+                for (var j = 0; j < BaseCounter; j++) {
+                    let x = sprites[i].image.x + (Math.random()-0.5)*sprites[i].image.width;
+                    let y = sprites[i].image.y + (Math.random()-0.5)*sprites[i].image.height;
+                    var new_stick = new Sprite("stick", x, y);
+                    sprites.push(new_stick);
+                }
+            } else if (sprites[i].type == "stack")  {
+                // make as many boxes as BaseCounter says
+                for (var j = 0; j < BaseCounter; j++) {
+                    let x = sprites[i].image.x + (Math.random()-0.5)*sprites[i].image.width;
+                    let y = sprites[i].image.y + (Math.random()-0.5)*sprites[i].image.height;
+                    var new_box = new Sprite("box", x, y);
+                    sprites.push(new_box);
+                }
             }
         }
     }
-    // then delete the boxes
-    let number_of_new_sticks = selected_boxes_indices.size * BaseCounter;
-    let backup_selected_sticks = selected_sticks_indices;
-    selected_sticks_indices = new Set();
-    deleteSelected();
-    selected_sticks_indices = backup_selected_sticks;
-
-    if (boxes.length == 0) {
-        lock("Unlocked");
+    
+    // then delete the old boxes/stacks
+    var new_sprites = [];
+    for (var i = 0; i < sprites.length; i++) {
+        if (selected_indices.has(i) && (sprites[i].type == "box" || sprites[i].type == "stack")) {
+            // box/stack is selected, so it gets deleted
+            sprites[i].image.destroy();
+            selected_indices.delete(i); // deselect its index
+        } else {
+            // sprite is not selected, so it gets kept
+            new_sprites.push(sprites[i]);
+        }
     }
+    sprites = new_sprites; // keep the new array of sprites
 }
